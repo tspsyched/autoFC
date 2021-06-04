733a3258ff63382e80f00eff6654e7aa3d62f6db
MTAmaranth <45687429+MTAmaranth@users.noreply.github.com>
2021-06-04 10:17:04 +0800

    Update get_iia.R
    Update 4th June. Roxygen docs added.

--- R/get_iia.R
+++ R/get_iia.R
@@ -1,3 +1,29 @@
+#' Helper Function for Outputting IIA Characteristics of Each Block
+#'
+#' @description This function prints IIA metrics for select items,
+#'              given the individual responses for the items.
+#'
+#' @usage get_iia(block, data)
+#'
+#'
+#' @param block An \emph{n} by \emph{k} integer matrix,
+#'              where \emph{n} is the number of item blocks
+#'              and \emph{k} is the number of items per block.
+#' @param data  A \emph{p} by \emph{m} numeric matrix with scores of
+#'              each of the \emph{p} participants for the \emph{m} items.
+#'
+#' @returns An \emph{n} by \emph{k} matrix indicating the four IIA metrics for each item block.
+#'
+#' @author Mengtong Li
+#'
+#' @examples
+#'   item_responses <- matrix(sample(seq(1:5), 600*60, replace = TRUE), ncol = 60, byrow = TRUE)
+#'   get_iia(matrix(seq(1:60), ncol = 3, byrow = TRUE), item_responses)
+#'
+#' @importFrom irrCAC bp.coeff.raw gwet.ac1.raw
+#' @export
+
+
 get_iia <- function(block, data) {
   results <- cbind(BPlin = c(), BPquad = c(), AClin = c(), ACquad = c())
   for (i in seq(1:nrow(block))) {
@@ -10,4 +36,3 @@ get_iia <- function(block, data) {
   }
   return(results)
 }
-
